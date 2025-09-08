4f00487dde0d7e15c8c13a9a9d2e1f6619a39447
SleptsovIvan <sl.vanya84@gmail.com>
2025-09-08 16:20:29 +0900

    character

---
+++ lib/config/router.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:rick_and_morty_lesson/features/details/view/details_view.dart';
+import 'package:rick_and_morty_lesson/features/home/view/home_view.dart';
+
+class AppRoutes {
+  static const String homePath = '/';
+  static const String detailsPath = '/details';
+}
+
+class AppRouterHelper {
+  static final AppRouterHelper _instance = AppRouterHelper._internal();
+
+  static late final GoRouter router;
+
+  static final GlobalKey<NavigatorState> parentNavigatorKey =
+      GlobalKey<NavigatorState>();
+
+  static AppRouterHelper get instance => _instance;
+
+  factory AppRouterHelper() {
+    return _instance;
+  }
+
+  AppRouterHelper._internal() {
+    final routes = [
+      GoRoute(
+        path: AppRoutes.homePath,
+        builder: (context, state) => HomeView(),
+      ),
+      GoRoute(
+        path: AppRoutes.detailsPath,
+        builder: (context, state) => DetailsView(),
+      ),
+    ];
+
+    router = GoRouter(
+      routes: routes,
+      initialLocation: AppRoutes.homePath,
+      navigatorKey: parentNavigatorKey,
+    );
+  }
+}
---
+++ lib/features/home/view/home_view.dart
@@ -0,0 +1,38 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty_lesson/config/constants.dart';
+import 'package:rick_and_morty_lesson/models/character/character.dart';
+import 'package:rick_and_morty_lesson/services/rick_and_morty_api/api.dart';
+
+class HomeView extends StatelessWidget {
+  HomeView({super.key});
+
+  final Dio dio = Dio();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            try {
+              final response = await RickAndMortyApiClient(
+                dio,
+                baseUrl: AppConstants.rickAndMortyApiUrl,
+              ).getCharacters();
+
+              response.results.forEach((Character character) {
+                inspect(character.name);
+              });
+            } catch (e) {
+              inspect(e);
+            }
+          },
+          child: Text('Get characters'),
+        ),
+      ),
+    );
+  }
+}
